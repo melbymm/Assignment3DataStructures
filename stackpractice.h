9262cfe085e81a660a958c55dbacb1086e1f71d1
Mitch <melby@chapman.edu>
2020-04-04 01:07:29 +0000

    Actually final

--- stackpractice.h
+++ stackpractice.h
@@ -4,7 +4,7 @@ using namespace std;
 template <class V>
 class GenStack{
   public:
-//establishs methods & variables
+
     GenStack();
     GenStack(int maxsize);
     ~GenStack();
@@ -17,57 +17,50 @@ class GenStack{
     bool isFull();
     int size();
     V *myStack;
-
+    //V *fullstack;
+    //string toString();
 
     int mSize;
     int top;
-
+    //bool isFull;
 
 
 };
-//default constructor
+
 template <class V>
 GenStack<V>::GenStack(){
   myStack = new V[200];
   mSize = 200;
   top = -1;
 }
-//overload conversation
 template <class V>
 GenStack<V>::GenStack(int maxsize){
   myStack = new V[maxsize];
   mSize = maxsize;
   top = -1;
 }
-//destructor
+
 template <class V>
 GenStack<V>::~GenStack(){
   delete myStack;
 }
-//push to stack
 template <class V>
 void GenStack<V>::push(V w){
-//create new stack if stack is full
   if(isFull()){
 
-    V* fullstack = new V[mSize * 2];
+    V* fullstack = new V[mSize *= 2];
 
     for(int i = 0; i < mSize; ++i){
       fullstack[i] = myStack[i];
     }
-//clear stack and set it equal to other stack
+
     delete myStack;
     myStack = fullstack;
-//double max size for new array
-    mSize = mSize * 2;
   }
-  //add variable to stack
     myStack[++top] = w;
 }
-//pop top of stack
 template <class V>
 void GenStack<V>::pop(){
-//exit it empty
   if(isEmpty()){
     cout << "Stack is empty" << endl;
     exit(1);
@@ -76,29 +69,24 @@ void GenStack<V>::pop(){
     myStack[--top];
   }
 }
-//peek at top of stack
 template <class V>
 V GenStack<V>::peek(){
-  //exit it empty
   if(isEmpty()){
-    cout << "Nothing here" << endl;
-    exit(1);
+    cout << "Stack is empty, could not peek. " << endl;
+
   }
   else{
     return myStack[top];
   }
 }
-//check if is empty
 template <class V>
 bool GenStack<V>::isEmpty(){
   return top == -1;
 }
-//check if full
 template <class V>
 bool GenStack<V>::isFull(){
   return top == mSize-1;
 }
-//get size
 template <class V>
 int GenStack<V>::size(){
   return top + 1;
